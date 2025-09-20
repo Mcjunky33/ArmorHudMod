e6ae28dc5a2c0fd9240116d39560380d9253544a
Mcjunky33 <stinkerle1102@gmail.com>
2025-09-20 10:50:40 +0200

    Add files via upload

---
+++ 1.21.1 source/src/client/java/de/mcjunky33/armor_hud/client/Armor_hudClient.java
@@ -0,0 +1,45 @@
+package de.mcjunky33.armor_hud.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.option.KeyBinding;
+import org.lwjgl.glfw.GLFW;
+import de.mcjunky33.armor_hud.client.config.ModMenuIntegration;
+import de.mcjunky33.armor_hud.client.config.ArmorHudConfig;
+
+public class Armor_hudClient implements ClientModInitializer {
+    public static KeyBinding keyBindOpenConfig;
+    public static KeyBinding keyBindToggleDurabilityMode;
+
+    @Override
+    public void onInitializeClient() {
+        keyBindOpenConfig = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.armorhud.open_config",
+                GLFW.GLFW_KEY_F10,
+                "key.categories.armorhud"
+        ));
+
+        keyBindToggleDurabilityMode = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                "key.armorhud.toggle_durability_mode",
+                GLFW.GLFW_KEY_UNKNOWN,
+                "key.categories.armorhud"
+        ));
+
+        HudRenderCallback.EVENT.register((context, tickDelta) -> {
+            new ArmorHudOverlay().renderArmorUI(context);
+        });
+
+        net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            if (keyBindOpenConfig.wasPressed()) {
+                client.setScreen(new ModMenuIntegration.SimpleConfigScreen(client.currentScreen));
+            }
+
+            if (keyBindToggleDurabilityMode.wasPressed()) {
+                ArmorHudConfig config = ArmorHudConfig.getInstance();
+                int newMode = (config.getDurabilityDisplayMode() + 1) % 3;
+                config.setDurabilityDisplayMode(newMode);
+            }
+        });
+    }
+}
---
+++ CHANGELOGver0.4.md
@@ -0,0 +1,23 @@
+# Changelog
+
+## [0.4] – Feature & Compatibility Update
+
+### English
+
+- **Minecraft 1.21 support:** Added full compatibility for Minecraft versions 1.21, 1.21.1, 1.21.6, 1.21.7, and 1.21.8.  
+  Support for versions 1.21.2 to 1.21.5 will be added soon.
+- **Dependency added:** The mod now requires [TranslucencyFix](https://modrinth.com/mod/translucencyfix/) for correct rendering on 1.21-1.21.1.
+- **Bug fixes:** Small bugfixes and improvements in the HUD renderer for better stability and visuals.
+- **Durability percentage:** You can now display armor durability as a percentage (%) in addition to the existing bar and number options.
+
+---
+
+### Deutsch
+
+- **Minecraft 1.21 Unterstützung:** Kompatibilität für Minecraft 1.21, 1.21.1, 1.21.6, 1.21.7 und 1.21.8 hinzugefügt.  
+  Die Unterstützung für 1.21.2 bis 1.21.5 folgt demnächst.
+- **Neue Abhängigkeit:** Die Mod benötigt nun [TranslucencyFix](https://modrinth.com/mod/translucencyfix/) für korrektes Rendering ab 1.21-1.21.1.
+- **Bugfixes:** Kleine Fehlerbehebungen und Verbesserungen im Renderer für mehr Stabilität und bessere Darstellung.
+- **Haltbarkeit in Prozent:** Die Haltbarkeit der Rüstung kann jetzt zusätzlich als Prozentwert (%) angezeigt werden, neben Balken und Zahl.
+
+---
